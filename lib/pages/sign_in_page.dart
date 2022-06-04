10be1ee8f0074226ac8a79588486e5717bed7406
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2022-06-04 10:08:16 +0700

    Slicing Sign In

--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -1,15 +1,96 @@
 import 'package:flutter/material.dart';
 import 'package:sepatu/pages/splash_page.dart';
+import 'package:sepatu/theme.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Sign In Page'),
+@override
+Widget build(BuildContext context) {
+  Widget header(){
+    return Container(
+      margin: EdgeInsets.only(top: 30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+        Text(
+          'Login',
+          style: primaryTextStyle.copyWith(
+            fontSize: 24,
+            fontWeight: semiBold,
+          ),
         ),
+        SizedBox(height: 2,),
+        Text(
+          'Sign In to Continue',
+          style: subtitletTextStyle,
+        ),
+      ],
+      ),
     );
   }
+
+Widget emailInput(){
+  return Container(
+    margin: EdgeInsets.only(top: 70),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Email Address', 
+        style: primaryTextStyle.copyWith(
+        fontSize: 16,
+        fontWeight: medium,
+        ),
+        ),
+        SizedBox(
+          height: 12,
+        ),
+        Container(
+          height: 50,
+          padding: EdgeInsets.symmetric(
+            horizontal: 16,
+          ),
+          decoration: BoxDecoration(
+            color: backgroundColor2,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Center(child: Row(
+            children: [
+              Image.asset('assets/icon_email.png', 
+              width: 17,
+              ),
+              SizedBox(
+                width: 16,
+              ),
+              Expanded(child: TextFormField(
+                style: primaryTextStyle,
+                decoration: InputDecoration.collapsed(
+                  hintText: 'Your Email Address',
+                  hintStyle: subtitletTextStyle,
+                ),
+              )),
+            ],
+          )),
+        ),
+      ],
+      ),
+  );
+}
+
+  return Scaffold(
+      backgroundColor: backgroundColor1,
+      body: SafeArea (
+        child: Container(
+        margin: EdgeInsets.symmetric(
+          horizontal: defaultMargin,
+        ),
+        child: Column(
+          children: [
+            header(),
+            emailInput(),
+          ],
+        ),
+        ),
+      ),
+    );
+}
 }
