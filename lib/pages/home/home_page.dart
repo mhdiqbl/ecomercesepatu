7c9322af73f1505ca2d2feef9d9c0802a060d018
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2022-06-08 14:07:29 +0700

    Slicing Home Page Bagian Header

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,12 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:sepatu/theme.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Page'),
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(
+        top: defaultMargin,
+        left: defaultMargin,
+        right: defaultMargin,
+      ),
+      child: Row(children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Hallo, Alex',
+                style: primaryTextStyle.copyWith(
+                  fontSize: 24,
+                  fontWeight: semiBold,
+                ),
+              ),
+              Text(
+                '@alexken',
+                style: subtitletTextStyle.copyWith(
+                  fontSize: 16,
+                )
+                ),
+            ],
+          ),
+        ),
+        Container(
+          width: 54,
+          height: 54,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(image: AssetImage('assets/image_profile.png'),
+            ),
+          ),
+        )
+      ]),
+      );
+    }
+
+    return ListView(
+      children: [
+        header(),
+      ],
     );
   }
 }
