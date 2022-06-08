ba54cd718781747ac7b4a0b1ab8d357e3cabdd9d
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2022-06-08 15:34:17 +0700

    Slicing Chat Page Bagian Content

--- lib/pages/home/chat_page.dart
+++ lib/pages/home/chat_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sepatu/theme.dart';
+import 'package:sepatu/widgets/chat_tile.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({Key? key}) : super(key: key);
@@ -22,57 +23,72 @@ class ChatPage extends StatelessWidget {
       );
     }
 
-    Widget content() {
-      return Expanded(
-        child: Container(
-          width: double.infinity,
-          color: backgroundColor3,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center, //centerkan gambar headset
-            children: [
-              Image.asset('assets/icon_headset.png',
-              width: 80,
-              ),
-              SizedBox(height: 20,),
-              Text('Opss no message yet', style: primaryTextStyle.copyWith(
-                fontSize: 16, 
-                fontWeight: medium,
-              ),
-              ),
-              SizedBox(height: 12,),
-              Text('You have never done a transaction',
-              style: secondaryTextStyle,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                height: 44,
-                child: TextButton(
-                  onPressed(){},
-                  style: TextButton.styleFrom(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 24,
-                      vertical: 10,
-                    ),
-                    backgroundColor: primaryColor,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12)
-                    ),
-                  ),
-                  child: Text('Explore Store',
-                  style: primaryTextStyle.copyWith(
-                    fontSize: 16,
-                    fontWeight: medium,
-                  ),),
-                  ),
-              )
-            ],
-        ),
-      ),
-      );
-    }
+    //   Widget emptyChat() {
+    //     return Expanded(
+    //       child: Container(
+    //         width: double.infinity,
+    //         color: backgroundColor3,
+    //         child: Column(
+    //           mainAxisAlignment: MainAxisAlignment.center, //centerkan gambar headset
+    //           children: [
+    //             Image.asset('assets/icon_headset.png',
+    //             width: 80,
+    //             ),
+    //             SizedBox(height: 20,),
+    //             Text('Opss no message yet', style: primaryTextStyle.copyWith(
+    //               fontSize: 16, 
+    //               fontWeight: medium,
+    //             ),
+    //             ),
+    //           SizedBox(height: 12,),
+    //           Text('You have never done a transaction',
+    //           style: secondaryTextStyle,
+    //           ),
+    //           SizedBox(
+    //             height: 20,
+    //           ),
+    //           Container(
+    //             height: 44,
+    //             child: TextButton(
+    //               onPressed(){},
+    //               style: TextButton.styleFrom(
+    //                 padding: EdgeInsets.symmetric(
+    //                   horizontal: 24,
+    //                   vertical: 10,
+    //                 ),
+    //                 backgroundColor: primaryColor,
+    //                 shape: RoundedRectangleBorder(
+    //                   borderRadius: BorderRadius.circular(12)
+    //                 ),
+    //               ),
+    //               child: Text('Explore Store',
+    //               style: primaryTextStyle.copyWith(
+    //                 fontSize: 16,
+    //                 fontWeight: medium,
+    //               ),),
+    //               ),
+    //           )
+    //         ],
+    //     ),
+    //   ),
+    //   );
+    // }
 
+Widget content(){
+  return Expanded(child: Container(
+    width: double.infinity,
+    color: backgroundColor3,
+    child: ListView(
+      padding: EdgeInsets.symmetric(
+        horizontal: defaultMargin,
+      ),
+      children: [
+        ChatTile(),
+      ],
+    ),
+  ),
+  );
+}
     return Column(
       children: [
         header(),
