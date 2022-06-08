1eb720d4796a14c316c12478a5b9c2333a8729f4
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2022-06-09 02:55:27 +0700

    Slicing Chat Page Bagian Input 1

--- lib/pages/detail_chat_page.dart
+++ lib/pages/detail_chat_page.dart
@@ -4,18 +4,20 @@ import 'package:sepatu/theme.dart';
 class DetailChatPage extends StatelessWidget {
   const DetailChatPage({Key? key}) : super(key: key);
 
+
   @override
-  Widget build(BuildContext context) {    
-    Widget header(){
+  Widget build(BuildContext context) {
+    Widget header() {
       return PreferredSize(
-        preferredSize: Size.fromHeight(70),
+        preferredSize: const Size.fromHeight(70),
         child: AppBar(
           backgroundColor: backgroundColor1,
           centerTitle: false,
           title: Row(
             children: [
-              Image.asset('assets/image_shop_logo_online.png',
-              width: 50,
+              Image.asset(
+                'assets/image_shop_logo_online.png',
+                width: 50,
               ),
               SizedBox(
                 width: 12,
@@ -23,29 +25,68 @@ class DetailChatPage extends StatelessWidget {
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text('Shoe Store',
-                  style: primaryTextStyle.copyWith(
-                    fontWeight: medium,
-                    fontSize: 14,
+                  Text(
+                    'Shoe Store',
+                    style: primaryTextStyle.copyWith(
+                      fontWeight: medium,
+                      fontSize: 14,
+                    ),
                   ),
-                  ),
-                  Text('Online',
-                  style: secondaryTextStyle.copyWith(
-                    fontWeight: light,
-                    fontSize: 14,
-                  ),
+                  Text(
+                    'Online',
+                    style: secondaryTextStyle.copyWith(
+                      fontWeight: light,
+                      fontSize: 14,
+                    ),
                   ),
                 ],
               ),
             ],
           ),
-        ), 
+        ),
+      );
+    }
+
+    Widget chatInput() {
+      return Container(
+        margin: EdgeInsets.all(20),
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                height: 45,
+                padding: EdgeInsets.symmetric(
+                  horizontal: 16,
+                ),
+                decoration: BoxDecoration(
+                  color: backgroundColor4,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Center(
+                  child: TextFormField(
+                    decoration: InputDecoration.collapsed(
+                      hintText: 'Type Message...',
+                      hintStyle: subtitletTextStyle,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Image.asset('assets/button_send.png',
+            width: 45,
+            )
+          ],
+        ),
       );
     }
 
     return Scaffold(
       backgroundColor: backgroundColor3,
       appBar: header(),
+      bottomNavigationBar: chatInput(),
     );
   }
 }
