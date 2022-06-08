2b90362ceb73da4d0791896e8e6305bbc4a1a1b0
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2022-06-08 13:58:02 +0700

    Slicing Halaman Main Page

--- lib/pages/home/main_page.dart
+++ lib/pages/home/main_page.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:sepatu/pages/home/chat_page.dart';
+import 'package:sepatu/pages/home/home_page.dart';
+import 'package:sepatu/pages/home/profile_page.dart';
+import 'package:sepatu/pages/home/wishlist_page.dart';
 import 'package:sepatu/theme.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
 
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     Widget cartButton() {
@@ -20,42 +30,97 @@ class MainPage extends StatelessWidget {
     Widget customButtomNav() {
       return ClipRRect(
         borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
-      child: BottomAppBar(
-        shape: CircularNotchedRectangle(),
-        notchMargin: 10,
-        clipBehavior: Clip.antiAlias,
-      child:BottomNavigationBar(
-        backgroundColor: backgroundColor4,
-        type: BottomNavigationBarType.fixed,
-        items: [
-        BottomNavigationBarItem(
-          icon: Image.asset('assets/icon_home.png',
-          width: 21,
+        child: BottomAppBar(
+          shape: CircularNotchedRectangle(),
+          notchMargin: 12,
+          clipBehavior: Clip.antiAlias,
+          child: BottomNavigationBar(
+            backgroundColor: backgroundColor4,
+            currentIndex: currentIndex,
+            onTap: (value) {
+              print(value);
+              setState(() {
+                currentIndex = value;
+              });
+            },
+            type: BottomNavigationBarType.fixed,
+            items: [
+              BottomNavigationBarItem(
+                icon: Container(
+                  margin: EdgeInsets.only(
+                    top: 20,
+                    bottom: 10),
+                  child: Image.asset(
+                    'assets/icon_home.png',
+                    width: 21,
+                    color: currentIndex == 0 ? primaryColor : Color(0xff808191),
+                  ),
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                  margin: EdgeInsets.only(
+                    top: 20,
+                    bottom: 10),
+                  child: Image.asset(
+                    'assets/icon_chat.png',
+                    width: 20,
+                    color: currentIndex == 1 ? primaryColor : Color(0xff808191),
+                  ),
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                  margin: EdgeInsets.only(
+                    top: 20,
+                    bottom: 10),
+                  child: Image.asset(
+                    'assets/icon_wishlist.png',
+                    width: 20,
+                    color: currentIndex == 2 ? primaryColor : Color(0xff808191),
+                  ),
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                  margin: EdgeInsets.only(
+                    top: 20,
+                    bottom: 10),
+                  child: Image.asset(
+                    'assets/icon_profile.png',
+                    width: 18,
+                    color: currentIndex == 3 ? primaryColor : Color(0xff808191),
+                  ),
+                ),
+                label: '',
+              ),
+            ],
+          ),
         ),
-        label: '',
-        ),
-        BottomNavigationBarItem(
-          icon: Image.asset('assets/icon_chat.png',
-          width: 20,
-        ),
-        label: '',
-        ),
-        BottomNavigationBarItem(
-          icon: Image.asset('assets/icon_wishlist.png',
-          width: 20,
-        ),
-        label: '',
-        ),
-        BottomNavigationBarItem(
-          icon: Image.asset('assets/icon_profile.png',
-          width: 18,
-        ),
-        label: '',
-        ),
-      ],
-      ),
-      ),
       );
+    }
+
+    Widget body() {
+      switch (currentIndex) {
+        case 0:
+          return HomePage();
+          break;
+        case 1:
+          return ChatPage();
+          break;
+        case 2:
+          return WishlistPage();
+          break;
+        case 3:
+          return ProfilePage();
+          break;
+
+        default:
+          return HomePage();
+      }
     }
 
     return Scaffold(
@@ -63,9 +128,7 @@ class MainPage extends StatelessWidget {
       floatingActionButton: cartButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: customButtomNav(),
-      body: Center(
-        child: Text('Main Page'),
-      ),
+      body: body(),
     );
   }
 }
