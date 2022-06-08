cc7cbc57a39e4674b2f57440fe08443355f936eb
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2022-06-09 02:38:31 +0700

    Slicing Chat Page Bagian header

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sepatu/pages/detail_chat_page.dart';
 import 'package:sepatu/pages/home/main_page.dart';
 import 'package:sepatu/pages/sign_in_page.dart';
 import 'package:sepatu/pages/splash_page.dart';
@@ -19,6 +20,7 @@ class MyApp extends StatelessWidget {
         '/sign-in' : (context) => SignInPage(),
         '/sign-up' : (context) => SignUpPage(),
         '/home': (context) => MainPage(),
+        '/detail-chat':(context) => DetailChatPage(),
       },
     );
   }
