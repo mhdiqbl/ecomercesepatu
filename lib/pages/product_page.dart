be551c39036cc215a6c93213a0e9a0d2d329b650
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2022-06-09 20:02:31 +0700

    Slicing Product Page Bagian Slider

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -1,12 +1,39 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:sepatu/theme.dart';
 
-class ProductPage extends StatelessWidget {
-  const ProductPage({Key? key}) : super(key: key);
+class ProductPage extends StatefulWidget {
+  @override
+  State<ProductPage> createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
+  List images = [
+    'assets/image_shoes.png',
+    'assets/image_shoes.png',
+    'assets/image_shoes.png',
+  ];
+
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    Widget indicator(int index) {
+      return Container(
+        width: currentIndex == index ? 16 : 4,
+        height: 4,
+        margin: EdgeInsets.symmetric(
+          horizontal: 2,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: currentIndex == index ? primaryColor : Color(0xffC4C4C4),
+        ),
+      );
+    }
+
     Widget header() {
+      int index = -1;
       return Column(
         children: [
           Container(
@@ -22,15 +49,48 @@ class ProductPage extends StatelessWidget {
                   onTap: () {
                     Navigator.pop(context);
                   },
-                child: Icon(
-                  Icons.chevron_left,
+                  child: Icon(
+                    Icons.chevron_left,
+                  ),
                 ),
-                ),
-                Icon(Icons.shopping_bag, color: backgroundColor1,
+                Icon(
+                  Icons.shopping_bag,
+                  color: backgroundColor1,
                 ),
               ],
             ),
-          )
+          ),
+          CarouselSlider(
+            items: images
+                .map(
+                  (image) => Image.asset(
+                    image,
+                    width: MediaQuery.of(context).size.width,
+                    height: 310,
+                    fit: BoxFit.cover,
+                  ),
+                )
+                .toList(),
+            options: CarouselOptions(
+                initialPage: 0,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                }),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: images.map(
+              (e) {
+                index++;
+                return indicator(index);
+              },
+            ).toList(),
+          ),
         ],
       );
     }
