8027e65ad921645b4a53ea9abf07d9f2eabece17
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2022-06-09 18:07:49 +0700

    Slicing Product Page Bagian Header Navigasi

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:sepatu/pages/detail_chat_page.dart';
 import 'package:sepatu/pages/edit_profile_page.dart';
 import 'package:sepatu/pages/home/main_page.dart';
+import 'package:sepatu/pages/product_page.dart';
 import 'package:sepatu/pages/sign_in_page.dart';
 import 'package:sepatu/pages/splash_page.dart';
 import 'package:sepatu/pages/sign_up_page.dart';
@@ -23,7 +24,8 @@ class MyApp extends StatelessWidget {
         '/home': (context) => MainPage(),
         '/detail-chat':(context) => DetailChatPage(),
         '/edit-profile': (context) => EditProfilePage(),
-      },
+        '/product':(context) => ProductPage(),
+        },
     );
   }
 }
